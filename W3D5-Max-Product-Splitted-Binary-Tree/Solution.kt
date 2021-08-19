a028d0b8d46a3d2aafec170d53e1b389c5cddba0
davidleiti <david.leiti@mindera.com>
2021-08-19 17:10:50 +0300

    Update W3D5 Solution

--- W3D5-Max-Product-Splitted-Binary-Tree/Solution.kt
+++ W3D5-Max-Product-Splitted-Binary-Tree/Solution.kt
@@ -25,7 +25,6 @@ class SolutionTree {
         val nodeSum = parentSum + node.`val`
         val leftSum = sum.left?.`val` ?: 0
         val rightSum = sum.right?.`val` ?: 0
-
         val product = if (leftSum < rightSum) {
             (leftSum + nodeSum) * rightSum
         } else {
@@ -33,41 +32,35 @@ class SolutionTree {
         }
 
         maxProduct = if (maxProduct >= product) maxProduct else product.toLong()
-
         node.left?.let { leftNode -> findMaxProduct(leftNode, nodeSum + rightSum, sum.left!!) }
         node.right?.let { rightNode -> findMaxProduct(rightNode, nodeSum + leftSum, sum.right!!) }
     }
 
     fun buildSumTree(node: TreeNode): TreeNode {
-        return if (node.left == null && node.right == null) {
-            TreeNode(node.`val`)
-        } else {
-            val leftNode = node.left?.let { buildSumTree(it) }
-            val rightNode = node.right?.let { buildSumTree(it) }
-            TreeNode(node.`val` + (leftNode?.`val` ?: 0) + (rightNode?.`val` ?: 0)).apply {
-                leftNode?.let { left = it }
-                rightNode?.let { right = it }
-            }
+        val leftNode = node.left?.let { buildSumTree(it) }
+        val rightNode = node.right?.let { buildSumTree(it) }
+        return TreeNode(node.`val` + (leftNode?.`val` ?: 0) + (rightNode?.`val` ?: 0)).apply {
+            leftNode?.let { left = it }
+            rightNode?.let { right = it }
         }
     }
 }
 
 class SolutionList {
-    
     fun maxProduct(root: TreeNode?): Int {
         return root?.let {
-            val sums = LinkedList<Long>()
+            val sums = LinkedList<Int>()
             val totalSum = getSums(root, sums)
             var maxProduct: Long = 0
             for (sum in sums) {
-                val product = sum * (totalSum - sum)
+                val product: Long = sum.toLong() * (totalSum - sum)
                 if (product > maxProduct) maxProduct = product
             }
             (maxProduct % 1_000_000_007).toInt()
         } ?: 0
     }
 
-    fun getSums(node: TreeNode, destination: MutableList<Long>): Long {
+    fun getSums(node: TreeNode, destination: MutableList<Int>): Int {
         val leftSum = node.left?.let { leftNode -> getSums(leftNode, destination) } ?: 0
         val rightSum = node.right?.let { rightNode -> getSums(rightNode, destination) } ?: 0
         val sum = node.`val` + leftSum + rightSum
